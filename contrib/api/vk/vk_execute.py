b2ac0c3f093ae7ad6db002ee86765e4c78ba5ccf
Alexey Proskuryakov <alexey.proskuryakov@gmail.com>
2014-09-15 03:24:55 +0400

    loading all photos

--- contrib/api/vk/vk_execute.py
+++ contrib/api/vk/vk_execute.py
@@ -51,7 +51,7 @@ class VK_API_Execute(VK_API):
                 "friends":API.friends.get({"user_id":f_user.id}),
                 "subscriptions":API.users.getSubscriptions({"user_id":f_user.id, "extended":1, "count":200}),
                 "followers":API.users.getFollowers({"user_id":f_user.id, "count":1000}),
-                "photos":API.photos.getAll({"owner_id":f_user.id, "count":200, "photo_sizes":1,"extended":1}),
+                "photos":API.photos.getAll({"owner_id":f_user.id, "count":1000, "photo_sizes":1,"extended":1}),
                 "photo_comments":API.photos.getAllComments({"owner_id":f_user.id, "need_likes":1, "count":100}),
                 "videos":API.video.get({"owner_id":f_user.id, "count":200, "extended":1}),
                 "wall":API.wall.get({"owner_id":f_user.id, "count":100}),
@@ -99,13 +99,80 @@ class VK_API_Execute(VK_API):
 
         return user, content_result
 
+    def get_photos_data(self, user_id):
+        """
+        here is vk script:
+
+var user_identity = Args.user_id;
+var user_id = API.users.get({"user_ids":user_identity})[0].id;
+var next_albums = Args.next_album_ids;
+var last_offset = Args.last_offset;
+var album_ids = null;
+if (next_albums){
+    album_ids = API.photos.getAlbums({"owner_id":user_id, "album_ids":next_albums}).items@.id;
+}else{
+    album_ids = API.photos.getAlbums({"owner_id":user_id, "need_system":1}).items@.id;
+}
+var album_counter = album_ids.length-1;
+var req_counter = 23;
+var photos = [];
+var album_batch = [];
+var offset = 0;
+if (last_offset){
+    offset = parseInt(last_offset);
+}
+var photos_at_album = offset+1;
+while (1){
+    while (photos_at_album - offset > 0){
+        var photos_object = API.photos.get({"owner_id":user_id, "album_id":album_ids[album_counter], "count":1000, "offset":offset, "extended":1});
+        req_counter = req_counter - 1;
+        album_batch.push(photos_object.items);
+        if (req_counter < 0){
+            photos.push(album_batch);
+            return {"photos":photos, "album_ids":album_ids, "next":album_ids.slice(0,album_counter),"last_offset":offset};
+        }
+        photos_at_album = photos_object.count;
+        offset = offset + 1000;
+    }
+    photos.push(album_batch);
+    album_batch = [];
+    photos_at_album = 1;
+    offset = 0;
+    album_counter = album_counter-1;
+    if (album_counter < 0){
+        return {"photos":photos, "album_ids":album_ids,"next":null, "last_offset":null};
+    }
+}
+
+
+        :param user_id: must be user sn_id not screen_name
+        :return: all photos from all albums
+        """
+        def form_photos_acc(photo_acc):
+            acc = []
+            for album_els in photo_acc:
+                for el in album_els:
+                    if isinstance(el, list):
+                        acc.extend(el)
+                    else:
+                        acc.append(el)
+            return acc
+
+        photos_data = self.get('execute.get_photos', **{'usr_id': user_id})
+        photos_acc = photos_data['photos']
+        while photos_data['next'] or photos_data['last_offset']:
+            photos_data = self.get('get_photos', **{'usr_id': user_id, 'next_album_ids': photos_data['next'],
+                                                    'last_offset': photos_data['last_offset']})
+            photos_acc.extend(photos_data['photos'])
+
+        result = form_photos_acc(photos_acc)
+        return result
+
 
 if __name__ == '__main__':
     # cr = ContentResult()
     # cri = ContentResultIntelligentRelations(1)
     # print isinstance(cri,cr.__class__)
     vk = VK_API_Execute()
-    user, content_result = vk.get_user_data('from_to_where')
-    print user
-    print content_result
+    vk.get_photos_data(114924709)
 
